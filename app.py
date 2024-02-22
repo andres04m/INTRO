92f6052f140f89263643c139860b56036d41c0ce
andres04m <160882189+andres04m@users.noreply.github.com>
2024-02-22 10:32:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import streamlit as st
 form PTL import Image
 
 st.title("Mi primera app!!")
+
+st.header("En este espacio comienzo a desarrollar mis apliaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend frontend.")
+image = Image.open('imagen1.jpeg')
+
