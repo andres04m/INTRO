cd3ccfd907415a988e9c61b1bc97e69b57c8d65d
andres04m <160882189+andres04m@users.noreply.github.com>
2024-02-22 10:33:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("Mi primera app!!")
 
 st.header("En este espacio comienzo a desarrollar mis apliaciones para interfaces multimodales")
 st.write("Facilmente puedo realizar backend frontend.")
-image = Image.open('imagen1.jpeg')
+image = Image.open('imagen1.jpg')
 
