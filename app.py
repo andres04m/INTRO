82fbc4d1eb450b18b8236dab10896a5da27ce5e8
andres04m <160882189+andres04m@users.noreply.github.com>
2024-02-22 10:34:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,4 @@ st.header("En este espacio comienzo a desarrollar mis apliaciones para interface
 st.write("Facilmente puedo realizar backend frontend.")
 image = Image.open('imagen1.jpg')
 
+st.image(image, caption= 'Interfaces multimodales')
